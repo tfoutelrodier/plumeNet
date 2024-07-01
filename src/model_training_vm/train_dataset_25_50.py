dd83125095667383cdc71e2e5714a501e4c0556b
Theo Foutel Rodier <theo.foutelrodier@orange.fr>
2024-07-01 17:47:11 +0200

    remove half completed loggin procedure. Will add it back in full later

--- src/model_training_vm/train_dataset_25_50.py
+++ src/model_training_vm/train_dataset_25_50.py
@@ -29,12 +29,12 @@ patience = 10
 random_state = 0
 
 # set logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename=os.path.join(logs_folder_,), encoding='utf-8', level=logging.DEBUG)
-logger.debug('This message should go to the log file')
-logger.info('So should this')
-logger.warning('And this, too')
-logger.error('And non-ASCII stuff, too, like Øresund and Malmö')
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(filename=os.path.join(logs_folder_,), encoding='utf-8', level=logging.DEBUG)
+# logger.debug('This message should go to the log file')
+# logger.info('So should this')
+# logger.warning('And this, too')
+# logger.error('And non-ASCII stuff, too, like Øresund and Malmö')
 
 
 
