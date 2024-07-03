cb88a5a6c1be212c3669c2b6125424cded92ad8f
Theo Foutel Rodier <theo.foutelrodier@orange.fr>
2024-07-03 17:45:43 +0200

    add script to run models on VM on bigger datasets

--- src/model_training_vm/train_dataset.py
+++ src/model_training_vm/train_dataset.py
@@ -2,11 +2,15 @@ import logging
 import os
 import sys
 
+import json
+import numpy as np
 import tensorflow as tf
+import keras
 from keras.utils import image_dataset_from_directory
-from keras.callbacks import EarlyStopping, TensorBoard
-from keras.layers import Dense, GlobalAveragePooling2D
-from keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
 
 
 # directiory with all images, one folder per class
@@ -52,54 +56,60 @@ def create_model(num_classes: int) -> 'Model':
     return model
 
 
-logger.info('Loading datasets traina and rest...')
+logger.info('Loading datasets train and rest...')
 ds_train = image_dataset_from_directory(dataset_dir, labels='inferred', image_size=(300, 300), validation_split=0.2, subset="training", seed=random_state, batch_size=batch_size)
 ds_test = image_dataset_from_directory(dataset_dir, labels='inferred', image_size=(300, 300), validation_split=0.2, subset="validation", seed=random_state, batch_size=batch_size)
 logger.info("Done !")
 
 # Obtenir le nombre de classes à partir de ds
+class_names = ds_train.class_names
 num_classes = len(ds_train.class_names)
+with open(f'{output_model_dir}/{dataset_name}_class_names.json', 'w') as f:
+    json.dump(class_names, f)
 
 # Convertir les étiquettes cibles en vecteurs one-hot
-logger.info('One-hot-encoding')
-ds_train = ds_train.map(lambda x, y: (x, tf.one_hot(y, num_classes)))
-ds_test = ds_test.map(lambda x, y: (x, tf.one_hot(y, num_classes)))
+logger.info('Preprocessing')
+preprocess_input = tf.keras.applications.resnet50.preprocess_input
+
+ds_train_preprocessed = ds_train.map(lambda x, y: (preprocess_input(x), y))
+ds_test_preprocessed = ds_test.map(lambda x, y: (preprocess_input(x), y))
 
 # Run the model
 tf.config.list_physical_devices()
 
-# model = create_model(num_classes)
-
 # Charger le modèle ResNet50 pré-entraîné avec les poids ImageNet
-base_model = tf.keras.applications.ResNet50V2(weights='imagenet', include_top=False, input_shape=(300, 300, 3))
+base_model = tf.keras.applications.ResNet50(weights='imagenet', include_top=False, input_shape=(300, 300, 3))
 
 # Ajouter une couche de pooling global pour réduire la dimensionalité
 x = base_model.output
 x_2 = GlobalAveragePooling2D()(x)
 
-x_3 = Dense(num_classes, activation='relu')(x_2)
+# x_3 = Dense(num_classes, activation='relu')(x_2)
+x_3 = Dense(512, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01))(x_2)
+x_3 = Dropout(0.5)(x_3)
 
 # Ajouter une couche dense pour effectuer la classification
 output = Dense(num_classes, activation='softmax')(x_3)
 
 # Créer le modèle final en combinant le modèle de base et les couches supplémentaires
-model = Model(inputs=base_model.input, outputs=output)
+model2 = Model(inputs=base_model.input, outputs=output)
 
 # Geler les couches du modèle de base pour éviter de les entraîner
 for layer in base_model.layers:
     layer.trainable = False
 
 # Compiler le modèle
-logger.info("Compiling model")
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.001), 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=[keras.metrics.SparseCategoricalAccuracy()])
 
-callbacks = [EarlyStopping(monitor='val_loss', patience=patience),
+callbacks = [EarlyStopping(monitor='val_loss', patience=10),
              TensorBoard(log_dir=f'{logs_dir}/{logs_folder_name}', histogram_freq=0, write_graph=True, write_images=True)]
 
 
 # Entraîner le modèle sur votre dataset avec le callback personnalisé
 logger.info('training_model')
-history = model.fit(ds_train, epochs=nb_epochs, callbacks=callbacks, validation_data=ds_test)
+history = model.fit(ds_train_preprocessed, epochs=10, callbacks=callbacks, validation_data=ds_test_preprocessed)
 
 # save model to pickle
 logger.info(f'saving model info: {model_save_file}')
--- src/model_training_vm/vm_setup.sh
+++ src/model_training_vm/vm_setup.sh
@@ -4,12 +4,12 @@
 # load dataset, install required packages, create required folders
 
 # main information
-DATASET_NAME="french_bird_db_25_50"
+DATASET_NAME="french_bird_db_400_800"
 
 BASE_DIR=$HOME  # should be home. It might beak the code if you change it
 
 # Datasets import
-DATASET_BUCKET="french_bird_db_25-50"
+DATASET_BUCKET="french_bird_db"
 DATASET_MOUNT_DIR="/mnt/dataset"
 DATASET_DIR=${BASE_DIR}/"dataset"
 
@@ -54,6 +54,7 @@ sudo apt install -y python3-pip
 
 # setup venv from built python version
 cd ${BASE_DIR}
+# two possible cases seen from test, try one then the other if first one fails
 ${BASE_DIR}/Python-${python_version}/Python-${python_version}/python -m venv ${VENV_DIR}  || ${BASE_DIR}/Python-${python_version}/python -m venv ${VENV_DIR}
 source .venv/bin/activate 
 
@@ -65,8 +66,8 @@ sudo mkdir ${MODEL_MOUNT_DIR}
 mkdir ${DATASET_DIR} ${MODEL_DIR} ${LOGS_DIR}
 
 # Get access to dataset and model buckets
-gsutil rsync -r gs://${DATASET_BUCKET} ${DATASET_MOUNT_DIR}
-gsutil rsync -r gs://${MODEL_BUCKET} ${MODEL_MOUNT_DIR}
+sudo gsutil rsync -r gs://${DATASET_BUCKET} ${DATASET_MOUNT_DIR}
+sudo gsutil rsync -r gs://${MODEL_BUCKET} ${MODEL_MOUNT_DIR}
 
 # untar dataset
 compressed_dataset_file=${DATASET_MOUNT_DIR}/${DATASET_NAME}.tgz
@@ -74,16 +75,14 @@ tar -xzf ${compressed_dataset_file} --directory ${DATASET_DIR}
 
 
 # potential requirements for tensorflow to run as efficiently as possible on CPU
-apt -y install nvidia-cudnn gcc g++ make 
+# sudo apt -y install nvidia-cudnn gcc g++ make 
 
 # install required packages and softwares
 git clone https://github.com/tfoutelrodier/plumeNet 
 # go to target branch with required scripts
-cd plumeNet
-git checkout feat/model_training_vm
+cd plumeNet && git checkout feat/model_training_vm && cd ${BASE_DIR}
 
 pip install -r ${SCRIPT_DIR}/requirements.txt
-
 
 # run model on dataset
 chmod 770 ${SCRIPT_DIR}/train_dataset_25_50.py
@@ -91,11 +90,5 @@ python ${SCRIPT_DIR}/train_dataset_25_50.py ${DATASET_DIR}/${DATASET_NAME} 10 50
 
 
 # save model
-gsutil rsync -r ${MODEL_DIR} gs://${MODEL_BUCKET}
+sudo gsutil rsync -r ${MODEL_DIR} gs://${MODEL_BUCKET}
 gcloud compute scp ${MODEL_DIR} ${VM_NAME}:${MODEL_MOUNT_DIR} --project=${PROJECT_ID} --zone=${ZONE}
-
-:/mount-directory
-
- --project=project-id
-
- --zone=zone
